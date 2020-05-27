1ce9b93f0dc20745f219edc0a64bdbdc495354d2
tallor <thomas.allor@protonmail.com>
2020-05-26 19:30:26 -0500

    Part 6 - Working on BasicMonster function

--- engine.py
+++ engine.py
@@ -1,5 +1,6 @@
 import tcod as libtcod
 
+from components.fighter import Fighter
 from entity import Entity, get_blocking_entities_at_location
 from fov_functions import initialize_fov, recompute_fov
 from game_states import GameStates
@@ -31,7 +32,8 @@ def main():
         'light_ground': libtcod.Color(200, 180, 50)
     }
 
-    player = Entity(0, 0, '@', libtcod.white, 'Player', blocks=True)
+    fighter_component = Fighter(hp = 30, defense = 2, power = 5)
+    player = Entity(0, 0, '@', libtcod.white, 'Player', blocks=True, fighter = fighter_component)
     entities = [player]
 
     libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
@@ -81,7 +83,7 @@ def main():
                 target = get_blocking_entities_at_location(entities, destination_x, destination_y)
 
                 if target: 
-                    print('You kick the' + target.name + ' in the shins, much to its annoyance!')
+                    player.fighter.attack(target)
                 else:
                     player.move(dx, dy)
 
@@ -99,8 +101,8 @@ def main():
 
         if game_state == GameStates.ENEMY_TURN:
             for entity in entities:
-                if entity != player:
-                    print('The ' + entity.name + ' ponders the meaning of its existence.')
+                if entity.ai:
+                    entity.ai.take_turn(player, fov_map, game_map, entities)
 
             game_state = GameStates.PLAYERS_TURN
 
